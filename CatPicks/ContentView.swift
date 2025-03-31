ff57fa32b242c5f583f79e336144dd43956b9edd
Roberrt0 <betovelez04@gmail.com>
2025-03-30 20:25:24 -0600

    Minor changes and fixes - Small comment deletion - Assets path fixed

--- CatPicks/ContentView.swift
+++ CatPicks/ContentView.swift
@@ -5,26 +5,6 @@
 //  Created by Luis Roberto Martinez on 25/11/24.
 //
 
-/* TODO
- 
- -> Save images UI
- -> Save images service
- -> add and improve animations
- -> Implement accessibility functions
- 
- *Notes*
- I dont know which component use for the Gallery UI, saved images
- must me able to be deleted and moved as in the photo app, user
- should also be able to download images and maybe other things.
- 
- planning to save images in the app´s directory, but i probably
- should look into alternatives just in case.
- 
- Look into alternatives to AsyncImage so i can implement smoother
- animations for the cat image.
- 
- */
-
 import SwiftUI
 
 struct ContentView: View {
